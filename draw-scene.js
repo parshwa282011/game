6cf2ea8ef6b257b1db22e546888bb375de25cadc
parshwa282011 <parswhashah282011@gamil.com>
2024-03-13 10:36:11 -0700

    e

--- draw-scene.js
+++ draw-scene.js
@@ -31,30 +31,30 @@ function drawScene(gl, programInfo, buffers, cubeRotation) {
   
     // Now move the drawing position a bit to where we want to
     // start drawing the square.
-    // mat4.translate(
-    //   modelViewMatrix, // destination matrix
-    //   modelViewMatrix, // matrix to translate
-    //   [-0.0, 0.0, -6.0]
-    // ); // amount to translate
+    mat4.translate(
+      modelViewMatrix, // destination matrix
+      modelViewMatrix, // matrix to translate
+      [-0.0, 0.0, -6.0]
+    ); // amount to translate
   
-    // mat4.rotate(
-    //   modelViewMatrix, // destination matrix
-    //   modelViewMatrix, // matrix to rotate
-    //   cubeRotation, // amount to rotate in radians
-    //   [0, 0, 1]
-    // ); // axis to rotate around (Z)
-    // mat4.rotate(
-    //   modelViewMatrix, // destination matrix
-    //   modelViewMatrix, // matrix to rotate
-    //   cubeRotation * 0.7, // amount to rotate in radians
-    //   [0, 1, 0]
-    // ); // axis to rotate around (Y)
-    // mat4.rotate(
-    //   modelViewMatrix, // destination matrix
-    //   modelViewMatrix, // matrix to rotate
-    //   cubeRotation * 0.3, // amount to rotate in radians
-    //   [1, 0, 0]
-    // ); // axis to rotate around (X)
+    mat4.rotate(
+      modelViewMatrix, // destination matrix
+      modelViewMatrix, // matrix to rotate
+      cubeRotation, // amount to rotate in radians
+      [0, 0, 0]
+    ); // axis to rotate around (Z)
+    mat4.rotate(
+      modelViewMatrix, // destination matrix
+      modelViewMatrix, // matrix to rotate
+      cubeRotation * 0.7, // amount to rotate in radians
+      [0, 0, 0]
+    ); // axis to rotate around (Y)
+    mat4.rotate(
+      modelViewMatrix, // destination matrix
+      modelViewMatrix, // matrix to rotate
+      cubeRotation * 0.3, // amount to rotate in radians
+      [0, 0, 0]
+    ); // axis to rotate around (X)
   
     // Tell WebGL how to pull out the positions from the position
     // buffer into the vertexPosition attribute.
